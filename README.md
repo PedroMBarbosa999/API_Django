f2cb146fa15fbaae1ec2fd4453b29c24bbf339f7
PedroMBarbosa <158504468+PedroMBarbosa@users.noreply.github.com>
2026-07-02 09:41:04 -0300

    preparação de arquivos auxiliares

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ API de um app mobile de administração de tarefas
 
 Paso a paso
 1. preparação do ambiente
-2. 
+2. preparação de arquivos auxilares (urls.py, models, seriallazer e etc.)
--- api/admtarefas/models.py
+++ api/admtarefas/models.py
@@ -1,3 +1,9 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class User(models.Model):
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.email
